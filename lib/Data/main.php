b02b6f1f4f74c47be7f835e99748aeaad7771cf2
Dimitri.Who <dimitri.who@gmail.com>
2015-12-26 15:32:01 +0100

    Fixed includes in Data

--- lib/Data/main.php
+++ lib/Data/main.php
@@ -4,12 +4,12 @@
 	include dirname(__FILE__)."/Controller.class.php";
 	include dirname(__FILE__)."/View.class.php";
 
-	include dirname(__FILE__)."/Field/Base.class.php";
-	include dirname(__FILE__)."/Field/ID.class.php";
-	include dirname(__FILE__)."/Field/dString.class.php";
-	include dirname(__FILE__)."/Field/Username.class.php";
-	include dirname(__FILE__)."/Field/Password.class.php";
-	include dirname(__FILE__)."/Field/Mail.class.php";
+	include dirname(__FILE__)."/field/Base.class.php";
+	include dirname(__FILE__)."/field/ID.class.php";
+	include dirname(__FILE__)."/field/dString.class.php";
+	include dirname(__FILE__)."/field/Username.class.php";
+	include dirname(__FILE__)."/field/Password.class.php";
+	include dirname(__FILE__)."/field/Mail.class.php";
 
 
 

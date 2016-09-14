96568db66215dc1dbef3728c0af30582078aa1fe
EchoWine <dimitri.who@gmail.com>
2016-09-14 23:37:23 +0200

    Added src/Assets

---
+++ src/Assets/Resources/public/http/http.js
@@ -0,0 +1,74 @@
+/**
+ * Http Request
+ */
+var http = {};
+
+/**
+ * Make ajax call
+ *
+ * @param {string} type
+ * @param {string} url
+ * @param {object} params
+ * @param {function} callback
+ */
+http.ajax = function(type,url,params = {},callback){
+	return $.ajax({
+		type: type,
+		url: url, 
+		data : params,
+		contentType: "application/x-www-form-urlencoded; charsetBySource=UTF-8",
+		success: function(response) {
+			callback(response);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log('Error during call: '+url);
+			console.log(errorThrown);
+			console.log(jqXHR);
+		},
+		dataType:'json'
+	});
+};
+
+/**
+ * Make get call
+ *
+ * @param {string} url
+ * @param {object} params
+ * @param {function} callback
+ */
+http.get = function(url,params,callback){
+	return http.ajax('GET',url+"?"+$.param(params),{},callback);
+};
+
+/**
+ * Make post call
+ *
+ * @param {string} url
+ * @param {object} params
+ * @param {function} callback
+ */
+http.post = function(url,params,callback){
+	return http.ajax('POST',url,params,callback);
+};
+
+/**
+ * Make put call
+ *
+ * @param {string} url
+ * @param {object} params
+ * @param {function} callback
+ */
+http.put = function(url,params,callback){
+	return http.ajax('PUT',url,params,callback);
+};
+
+/**
+ * Make delete call
+ *
+ * @param {string} url
+ * @param {object} params
+ * @param {function} callback
+ */
+http.delete = function(url,params,callback){
+	return http.ajax('DELETE',url,params,callback);
+};
